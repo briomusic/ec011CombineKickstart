d67c5c5cd047b4d92d6b60a65d3c920b26e6c8d9
Brio Taliaferro <briomusic@gmail.com>
2022-06-01 15:55:27 +0200

    observe in didSet

--- 01/_StartHere/Forest/Forest/ViewController.swift
+++ 01/_StartHere/Forest/Forest/ViewController.swift
@@ -2,8 +2,15 @@ import UIKit
 
 class ViewController: UIViewController {
   @IBOutlet private weak var label: UILabel!
+	
+	private var value = 0 {
+		didSet {
+			label.text = value.description
+		}
+	}
   
   @IBAction private func next(_ sender: UIButton) {
+	  value = Int.random(in: 1...100)
   }
 }
 
