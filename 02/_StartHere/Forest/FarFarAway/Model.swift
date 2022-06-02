e4c5bfe7a1a2b2e47d5efa75ad23bf2480b570ed
Brio Taliaferro <briomusic@gmail.com>
2022-06-02 15:14:13 +0200

    add model in framework

---
+++ 02/_StartHere/Forest/FarFarAway/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  FarFarAway
+//
+//  Created by Brio Taliaferro on 02.06.22.
+//
+
+import Foundation
+
+public struct Model {
+	public let value: Int
+	
+	public init(value: Int) {
+		self.value = value
+	}
+}
