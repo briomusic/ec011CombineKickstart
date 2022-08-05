da9b10fea305ae68a4b1b529672758465d3ac689
Brio Taliaferro <briomusic@gmail.com>
2022-08-05 11:57:36 +0200

    convert to SwiftUI

---
+++ 02/_StartHere/Forest/Forest/MainView.swift
@@ -0,0 +1,25 @@
+//
+//  MainView.swift
+//  Forest
+//
+//  Created by Brio Taliaferro on 05.08.22.
+//
+
+import Foundation
+import SwiftUI
+
+struct MainView {
+}
+
+
+extension MainView: View {
+	var body: some View {
+		VStack(spacing: 80) {
+			Text("...")
+			Button("Next",
+				   action: {
+				print("")
+			})
+		}
+	}
+}
